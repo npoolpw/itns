219e339b3d5608af9e73996097ecb16f9ce4fa6e
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-01 09:22:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
-var api = "http://apihost.com:port";
+var api = "http://itns.npool.pw:8119";
 
-var poolHost = "apihost.com";
+var poolHost = "itns.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/coin";
+var blockExplorer = "http://intensecoin.com/explorer/";
 
 var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://intensecoin.com/explorer/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://intensecoin.com/explorer/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"],
+        ["intensecoin.com/pool", "http://45.32.171.89:8111"],
+        ["itns.globalpool.cc", "http://111.231.72.116:8102"],
+        ["itns.luckypool.io", "https://itns.luckypool.io/api"],
+        ["intense.miningpools.us", "http://intense.miningpools.us:8117"],
+        ["pool.intensecoin.net", "https://pool.intensecoin.net/api"],
+        ["npool.pw/itns", "http://itns.npool.pw:8119"]
 ];
